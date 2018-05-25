a802e191a1c16945be3b1372bfc9269b78ef4241
Jesus Lizama <dev@hellojeb.us>
2018-05-24 21:58:47 -0700

    add more commenting

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 //handles conversions between Celsius and Fahrenheit
 package go_temp_conv
 
+//create custom types to prevent erroneous
+//arithmetic between Celsius and Fahrenheit values
+//as they are not equal in base
 type Celsius float64
 type Fahrenheit float64
 
