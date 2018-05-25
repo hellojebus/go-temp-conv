9395a25619ff2d957a3cd92733fee01c9e111bd6
Jesus Lizama <dev@hellojeb.us>
2018-05-24 21:57:23 -0700

    add commenting

--- conv.go
+++ conv.go
@@ -1,5 +1,8 @@
 package go_temp_conv
 
+//these functions are accessible throughout this package
+//because the methods start with uppercase names
+
 func CtoF(c Celsius) Fahrenheit {
 	return Fahrenheit(c * 9 / 5 + 32)
 }
